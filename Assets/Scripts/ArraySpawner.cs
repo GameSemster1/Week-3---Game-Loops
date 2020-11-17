632840986d6c5ec9533c8af1fac24df2b2258fb5
Elisha Azaria <elishadar@gmail.com>
2020-11-17 11:51:31 +0200

    Added cars

---
+++ Assets/Scripts/ArraySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Repeatedly spawns a random GameObject from a given array.
+/// </summary>
+public class ArraySpawner : MonoBehaviour
+{
+	[Tooltip("The GameObject to spawn.")] public GameObject[] toSpawn;
+
+	[Tooltip("The spawning rate (per second).")]
+	public float rate = 1;
+
+	[Tooltip("The variance of the spawning rate (per second).")]
+	public float randomVariance = 0;
+
+	private void Start()
+	{
+		StartCoroutine(SpawningCorutine());
+	}
+
+	/// <summary>
+	/// Repeatedly spawns a random GameObject from <see cref="ArraySpawner.toSpawn"/>,
+	/// waiting between '<see cref="ArraySpawner.rate"/> - <see cref="ArraySpawner.randomVariance"/>'
+	/// and '<see cref="ArraySpawner.rate"/> + <see cref="ArraySpawner.randomVariance"/>'
+	/// before every spawn.
+	/// </summary>
+	private IEnumerator SpawningCorutine()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(rate + randomVariance * (1 - Random.value * 2));
+
+			Instantiate(toSpawn[Random.Range(0, toSpawn.Length)], transform.position, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves the current GameObject at a set speed.
+/// </summary>
+public class Mover : MonoBehaviour
+{
+	[Tooltip("The speed to move this GameObject by, in every axis.")]
+	public Vector3 speed;
+
+	private void Update()
+	{
+		transform.Translate(speed * Time.deltaTime);
+	}
+}
